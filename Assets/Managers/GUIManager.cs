203c75fe812509f63f750774c22ceb281e4bd27c
Frank DiRosa IV <frankdirosaiv@gmail.com>
2016-09-10 22:33:23 -0500

    cube runner game

---
+++ Assets/Managers/GUIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+    public GUIText gameOverText, instructionsText, runnerText;
+    private Vector3 startPosition;
+
+
+    void Start ()
+    {
+        GameEventManager.GameStart += GameStart;
+        GameEventManager.GameStart += GameOver;
+        gameOverText.enabled = false;
+    }
+
+    void Update ()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            GameEventManager.TriggerGameStart();
+        }
+    }
+
+    private void GameStart ()
+    {
+        gameOverText.enabled = false;
+        instructionsText.enabled = false;
+        runnerText.enabled = false;
+        enabled = false;
+    }
+
+    private void GameOver()
+    {
+        gameOverText.enabled = true;
+        instructionsText.enabled = true;
+        enabled = true;
+    }
+
+}
---
+++ Assets/Runner/Runner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+
+    public static float distanceTraveled;
+
+    public float acceleration;
+
+    public Vector3 jumpVelocity;
+
+    public float gameOverY;
+
+    private bool touchingPlatform;
+
+
+    void Update () {
+        if (touchingPlatform && Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().AddForce(jumpVelocity, ForceMode.VelocityChange);
+            touchingPlatform = false;
+        }
+        distanceTraveled = transform.localPosition.x;
+
+        if (transform.localPosition.y < gameOverY)
+        {
+            GameEventManager.TriggerGameOver();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (touchingPlatform)
+        {
+            GetComponent<Rigidbody>().AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+        }
+    }
+
+    void OnCollisionEnter ()
+    {
+        touchingPlatform = true;
+    }
+
+    void OnCollisionExit ()
+    {
+        touchingPlatform = false;
+    }
+
+}
